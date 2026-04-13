50f07f133e926d0edb1ae10cbbd4776a335262a0
SanduniHerath <sandunisathsarani09@gmail.com>
2026-04-13 10:18:08 +0530

    Initial Xcode project setup

---
+++ GreenThumb/GreenThumb/GreenThumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbApp.swift
+//  GreenThumb
+//
+//  Created by Sanduni on 2026-04-13.
+//
+
+import SwiftUI
+
+@main
+struct GreenThumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreenThumb/GreenThumbTests/GreenThumbTests.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbTests.swift
+//  GreenThumbTests
+//
+//  Created by Sanduni on 2026-04-13.
+//
+
+import Testing
+@testable import GreenThumb
+
+struct GreenThumbTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
